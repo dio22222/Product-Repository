7d1146d06fd04fbda1143614b95607eb22be37da
dio22222 <dennisnklp@gmail.com>
2023-01-11 18:38:01 +0200

    Track Selected Products State in Product List

--- Client/src/components/Product.jsx
+++ Client/src/components/Product.jsx
@@ -1,17 +1,39 @@
+import { useState, useEffect, useRef } from 'react';
 import '../assets/styles/Product.scss'
 
 const Product = (props) => {
-    return (
-        <div className="product d-flex flex-column p-4 m-3">
-          <div className='d-flex'>
-            <input type="checkbox" name="" id="" className='' />
-            <div className='sku'>{props.sku}</div>
-          </div>
-          <h5 className='name m-0'>{props.name}</h5>
-          <div className='price'>{props.price}</div>
-          <div className='attribute'>{props.attribute.title}: {props.attribute.amount}{props.attribute.unit}</div>
-        </div>
-    );
+
+  const isFirstRender = useRef(true)
+
+  const [checked, setChecked] = useState(false)
+
+  const handleCheck = (e) => {
+    setChecked(e.target.checked)
+  }
+
+  useEffect(() => {
+    // Don't handle Initial Checked State (false)
+    if (isFirstRender.current) {
+      isFirstRender.current = false
+      return
+    }
+    
+    // Lift State to ProductList
+    props.handleProductSelection(props.sku, checked)
+
+  }, [checked])
+
+  return (
+    <div className="product d-flex flex-column p-4 m-3">
+      <div className='d-flex'>
+        <input type="checkbox" name="" onChange={handleCheck} />
+        <div className='sku'>{props.sku}</div>
+      </div>
+      <h5 className='name m-0'>{props.name}</h5>
+      <div className='price'>{props.price}</div>
+      <div className='attribute'>{props.attribute.title}: {props.attribute.amount}{props.attribute.unit}</div>
+    </div>
+  );
 };
 
 export default Product;
--- Client/src/components/ProductList.jsx
+++ Client/src/components/ProductList.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
 import Product from '../components/Product'
 import '../assets/styles/ProductList.scss'
 
 function ProductList(props) {
+
+    const [selectedProducts, setSelectedProducts] = useState([])
+
+    const SelectProductHandler = (sku, checked) => {
+        setSelectedProducts((previousState) => {
+            // This is Error Prone because it assumes that the initial checked state is false and that all product skus are unique
+            if (checked) return [...previousState, sku]
+            else return previousState.filter(product => product !== sku)
+        })
+    }
+
+    useEffect(() => {
+        console.log(selectedProducts)
+    }, [selectedProducts])
+
     return (
         <div className="product-list container-fluid d-flex flex-wrap justify-content-center">
               {props.productList.map((product, index) => (
@@ -9,7 +25,8 @@ function ProductList(props) {
                          sku={product.sku}
                          name={product.name}
                          price={product.price}
-                         attribute={product.attribute} />
+                         attribute={product.attribute} 
+                         handleProductSelection={SelectProductHandler}/>
               ))}
         </div>
     );
