b0d57ed2da1cd43cba3185ffab90359f8598496c
dio22222 <dennisnklp@gmail.com>
2023-01-12 12:05:12 +0200

    Delete Button Logic on Total Products Selected

--- Client/src/components/NavButtons.jsx
+++ Client/src/components/NavButtons.jsx
@@ -1,23 +1,28 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import SelectedProductsContext from '../context/selectedProducts'
 
 import '../assets/styles/NavButtons.scss'
 
 const NavButtons = (props) => {
-  return (
-  <>
-    {props.path === '/' ? 
+  const { totalProductsSelected } = useContext(SelectedProductsContext)
+
+  if (props.path === '/') {
+    return (
       <div className="nav-buttons">
         <Link to="add-product">Add Product</Link>
-        <button className='btn btn-danger'>Mass Delete</button>
+        <button className='btn btn-danger' disabled={ totalProductsSelected === 0 }>
+          { totalProductsSelected > 1 ? 'Mass Delete' : 'Delete' }
+        </button>
       </div>
-      :
-      <div className="nav-buttons">
-        <button className='btn btn-success'>Save</button>
-        <Link replace to={'/'} className='btn btn-danger text-light'>Cancel</Link>
-      </div>
-      
-     } 
-  </>
+    )
+  }
+
+  return (
+    <div className="nav-buttons">
+      <button className='btn btn-success'>Save</button>
+      <Link replace to={'/'} className='btn btn-danger text-light'>Cancel</Link>
+    </div>
   )
 }
 
