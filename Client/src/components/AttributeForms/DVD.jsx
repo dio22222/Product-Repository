4c14142d87df354152ba18bab5c696d6a3b7d877
dio22222 <dennisnklp@gmail.com>
2023-01-17 18:16:10 +0200

    Replaced Number inputs with Text inputs
    This is for easier validation handling. Number inputs are problematic.

--- Client/src/components/AttributeForms/DVD.jsx
+++ Client/src/components/AttributeForms/DVD.jsx
@@ -2,7 +2,7 @@ const DVD = (props) => {
   return (
       <div className="mb-3">
         <label htmlFor="size" className="form-label">Size (MB)</label>
-        <input type="number" className="form-control" id="size" step={1} onChange={props.onFormValueChange} aria-describedby="size_help_text"/>
+        <input type="text" className="form-control" id="size" onChange={props.onFormValueChange} aria-describedby="size_help_text"/>
         <div id="size_help_text" className="form-text">Please provide the Size in MB. In case of a large sizes keep in mind that 1GB = ~1000MB</div>
       </div>
   );
--- Client/src/components/AttributeForms/Furniture.jsx
+++ Client/src/components/AttributeForms/Furniture.jsx
@@ -3,15 +3,15 @@ const Furniture = (props) => {
     <>
     <div className="mb-3">
       <label htmlFor="height" className="form-label">Height (CM)</label>
-      <input type="number" className="form-control" id="height" step={1} onChange={props.onFormValueChange}/>
+      <input type="text" className="form-control" id="height" onChange={props.onFormValueChange}/>
     </div>
     <div className="mb-3">
       <label htmlFor="width" className="form-label">Width (CM)</label>
-      <input type="number" className="form-control" id="width" step={1}/>
+      <input type="text" className="form-control" id="width" onChange={props.onFormValueChange}/>
     </div>
     <div className="mb-3">
       <label htmlFor="length" className="form-label">Length (CM)</label>
-      <input type="number" className="form-control" id="length" step={1} aria-describedby="dimensions_help_text"/>
+      <input type="text" className="form-control" id="length" onChange={props.onFormValueChange} aria-describedby="dimensions_help_text"/>
     </div>
     <div id="dimensions_help_text" className="form-text">Please provide dimensions in centimeters. In case of large numbers keep in mind that 1m = 1000cm</div>
     </>
